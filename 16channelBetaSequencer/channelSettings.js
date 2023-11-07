a87ca46a65250d9aa24e35b658d69d5e4568bece
blockchainbrighton <jim@blockchainbrighton.com>
2023-11-07 15:56:47 +0000

    Trying to finish the audio trim svelte component

--- 16channelBetaSequencer/channelSettings.js
+++ 16channelBetaSequencer/channelSettings.js
@@ -21,3 +21,45 @@ function setChannelVolume(channelIndex, volume) {
     gainNode.gain.value = volume;
     }
 
+// Function to toggle the dropdown menu
+function toggleDropdownMenu(event) {
+  // Prevent the default action to avoid any unwanted behavior
+  event.preventDefault();
+
+  // Find the dropdown content related to the clicked arrow
+  const dropdownContent = event.target.closest('.channel-dropdown').querySelector('.channel-dropdown-content');
+  
+  // Check if the dropdown content is found
+  if (dropdownContent) {
+    // Toggle the display property
+    dropdownContent.style.display = dropdownContent.style.display === 'block' ? 'none' : 'block';
+  } else {
+    // If the dropdown content is not found, log an error message
+    console.error('Dropdown content not found');
+  }
+}
+
+// Event listener for the dropdown arrow
+document.addEventListener('click', function(event) {
+  // Check if the clicked element is the dropdown arrow or any of its children
+  if (event.target.matches('.channel-dropdown-arrow') || event.target.closest('.channel-dropdown-arrow')) {
+    toggleDropdownMenu(event);
+  } else {
+    // If the click is outside the dropdown, close any open dropdown menus
+    const dropdowns = document.querySelectorAll('.channel-dropdown-content');
+    dropdowns.forEach(function(dropdown) {
+      if (dropdown.style.display === 'block') {
+        dropdown.style.display = 'none';
+      }
+    });
+  }
+}, true); // Use capturing to ensure this runs before the arrow's click event
+
+// Event listener for the trim audio option
+document.addEventListener('click', function(event) {
+  if (event.target.matches('.trim-audio-option')) {
+    openTrimModule();
+    // Hide the dropdown menu after selection
+    event.target.closest('.channel-dropdown-content').style.display = 'none';
+  }
+}, true);
