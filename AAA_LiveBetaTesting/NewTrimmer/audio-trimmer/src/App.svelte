3f16409dc93962a1bff6242a4e8f63ca9cb2ab71
blockchainbrighton <jim@blockchainbrighton.com>
2023-11-30 01:38:27 +0000

    updates

--- AAA_LiveBetaTesting/NewTrimmer/audio-trimmer/src/App.svelte
+++ AAA_LiveBetaTesting/NewTrimmer/audio-trimmer/src/App.svelte
@@ -1,8 +1,30 @@
 <script>
-	import AudioTrimmer from './AudioTrimmer.svelte';
-  </script>
+  import AudioTrimmer from './AudioTrimmer.svelte';
+  let showModal = false;
+
+  function openModal() {
+      showModal = true;
+  }
+
+  function closeModal() {
+      showModal = false;
+  }
   
-  <main>
-	<AudioTrimmer />
-  </main>
-  
+  function handleKeydown(event) {
+        // Check if the key pressed is 'Enter' or 'Space'
+        if (event.key === 'Enter' || event.key === ' ') {
+            closeModal();
+        }
+    }
+</script>
+
+<button on:click={openModal}>Open Audio Trimmer</button>
+
+<div id="audio-trimmer-modal" class="modal" style="display: {showModal ? 'block' : 'none'};">
+  <div class="modal-content">
+      <span class="close-button" on:click={closeModal}>&times;</span>
+      {#if showModal}
+          <AudioTrimmer />
+      {/if}
+  </div>
+</div>
