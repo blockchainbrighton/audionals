b7e9fbc159bc9ea93946d104d949a4d67d07a64f
blockchainbrighton <jim@blockchainbrighton.com>
2023-12-18 21:11:07 +0000

    updates

--- New_Audional_Sequencer/createStepButtonsForSequence.js
+++ New_Audional_Sequencer/createStepButtonsForSequence.js
@@ -5,38 +5,28 @@ function createStepButtonsForSequence() {
     console.log("[createStepButtonsForSequence] [SeqDebug] entered");
     channels.forEach((channel, channelIndex) => {
         const stepsContainer = channel.querySelector('.steps-container');
+        const loadSampleButton = channel.querySelector('.load-sample-button');
+        const colorClass = loadSampleButton.className.match(/\bcolor-[^ ]+/) || ''; // Extract color class
         stepsContainer.innerHTML = '';
-    
+
         let currentSequence = window.unifiedSequencerSettings.settings.masterSettings.currentSequence;
-    
+
         for (let i = 0; i < 64; i++) {
             const button = document.createElement('button');
             button.classList.add('step-button');
+            if (colorClass) {
+                button.classList.add(colorClass[0]); // Apply color class
+            }
             button.id = `Sequence${currentSequence}-ch${channelIndex}-step-${i}`;
 
             button.addEventListener('click', () => {
                 let currentStepState = window.unifiedSequencerSettings.getStepState(currentSequence, channelIndex, i);
                 window.unifiedSequencerSettings.updateStepState(currentSequence, channelIndex, i, !currentStepState);
-
-                // Toggle the 'selected' class and update color based on the loadSampleButton's color
-                if (button.classList.toggle('selected')) {
-                    const loadSampleButton = channel.querySelector('.load-sample-button');
-                    const colorClass = loadSampleButton.className.match(/\bcolor-[^ ]+/);
-                    if (colorClass) {
-                        button.classList.add(colorClass[0]);
-                    } else {
-                        // If no color class, ensure default color is used
-                        button.style.backgroundColor = 'var(--accent-color)';
-                    }
-                } else {
-                    button.classList.remove(...button.classList);
-                    button.classList.add('step-button'); // Re-add the default class
-                    button.style.backgroundColor = ''; // Remove inline style if any
-                }
-
+                button.classList.toggle('dynamic-color');
+ 
+                // Update the UI for the specific step
                 updateSpecificStepUI(currentSequence, channelIndex, i);
             });
-
             stepsContainer.appendChild(button);
         }
 
@@ -45,3 +35,23 @@ function createStepButtonsForSequence() {
 }
 
 document.addEventListener('DOMContentLoaded', createStepButtonsForSequence);
+
+function updateSelectedStepButtonColor() {
+    const styleSheet = document.styleSheets[0]; // Reference to the first stylesheet
+    const loadSampleButton = document.querySelector('.load-sample-button');
+    const loadSampleButtonColor = getComputedStyle(loadSampleButton).backgroundColor;
+    const className = '.step-button.selected.dynamic-color';
+
+    // Remove existing rule if it exists
+    Array.from(styleSheet.cssRules).forEach((rule, index) => {
+        if (rule.selectorText === className) {
+            styleSheet.deleteRule(index);
+        }
+    });
+
+    // Add new rule
+    if (loadSampleButtonColor) {
+        const newRule = `${className} { background-color: ${loadSampleButtonColor}; }`;
+        styleSheet.insertRule(newRule, styleSheet.cssRules.length);
+    }
+}
