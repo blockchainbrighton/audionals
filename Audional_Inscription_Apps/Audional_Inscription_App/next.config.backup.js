2ce47143fbcc44f9a13a20e46adfb28d4714d4b9
jim.btc <47526689+blockchainbrighton@users.noreply.github.com>
2024-11-29 21:49:29 +0000

    config

--- Audional_Inscription_Apps/Audional_Inscription_App/next.config.backup.js
+++ Audional_Inscription_Apps/Audional_Inscription_App/next.config.backup.js
@@ -3,7 +3,7 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   basePath: '/Audional_Inscription_Apps/Audional_Inscription_App',
-  assetPrefix: '/Audional_Inscription_Apps/Audional_Inscription_App',
+  assetPrefix: '/Audional_Inscription_Apps/Audional_Inscription_App', // Optional but recommended
 };
 
 module.exports = nextConfig;
