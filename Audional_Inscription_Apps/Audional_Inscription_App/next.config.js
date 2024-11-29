bbba69fb65984ce6b22ca8045b0d8f5f1240d725
jim.btc <47526689+blockchainbrighton@users.noreply.github.com>
2024-11-29 16:14:52 +0000

    updating next.config

--- Audional_Inscription_Apps/Audional_Inscription_App/next.config.js
+++ Audional_Inscription_Apps/Audional_Inscription_App/next.config.js
@@ -1,8 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true, // Enable React Strict Mode for better development feedback
-  swcMinify: true, // Enable faster builds with SWC
-  basePath: '/audinalinscriptionapps/audinalinscriptionapp', // Set the subdirectory path
+  // Add your config options here
 };
 
 module.exports = nextConfig;
