49f9424df63ec77bcb5baafab0386256d9b1f1cd
Jim.btc <jim@audionals.com>
2025-05-29 21:27:47 +0100

    1.02

--- opus-file-generator/ab-player.js
+++ opus-file-generator/ab-player.js
@@ -1,7 +1,7 @@
 // ab-player.js
 
-window.AB_PLAYER_VERSION = '1.02';
-console.log('[A/B Player] Loading AB Player Version 1.01');
+// window.AB_PLAYER_VERSION = '1.02';
+console.log('[A/B Player] Loading AB Player Version 1.02');
 
 
 /**
