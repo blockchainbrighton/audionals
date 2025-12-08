2a915cf11120dfa062a494a5a269a8703c24a26f
jim.btc <jim@audionals.com>
2025-12-08 12:37:39 +0000

    stacks login

--- stacks-login/stacks-login-game/vite.config.js
+++ stacks-login/stacks-login-game/vite.config.js
@@ -2,20 +2,7 @@ import { defineConfig } from 'vite';
 import { nodePolyfills } from 'vite-plugin-node-polyfills';
 
 export default defineConfig({
-  // 1. Force 'process' and 'global' to exist
-  define: {
-    'process.env': {}, 
-    global: 'window',
-  },
-  resolve: {
-    // 2. Alias these to browser-compatible versions
-    alias: {
-      stream: 'stream-browserify',
-      zlib: 'browserify-zlib',
-      util: 'util'
-    },
-  },
-  // 3. Load the polyfill plugin
+  // Enable the Polyfill Plugin
   plugins: [
     nodePolyfills({
       globals: {
